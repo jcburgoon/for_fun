b85ed8ec81f05416a0b075e67ac55a44cb4f55c6
Jesse <jesse.burgoon@gmail.com>
2017-08-16 15:18:23 -0400

    Update et_xml.py

--- et_xml.py
+++ et_xml.py
@@ -7,14 +7,6 @@ tree = ET.parse('/tmp/tools.nessus')
 
 root = tree.getroot()
 
-# one way to get the IP
-#for ip in root[1]:
-#    print ip.attrib
-
-# another way to get the IP
-#for tag in root[1].findall('./ReportHost/HostProperties/*[@name="host-ip"]'):
-#    print tag.text
-
 # get all report hosts -> needed to extract IP address
 for host in root[1].findall('./ReportHost'):
     # get all report items and report plugins that are not Info
