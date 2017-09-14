3efc734f1bc95af2da126ec9f7414bba460cd7a0
Jesse <jesse.burgoon@gmail.com>
2017-09-14 15:06:34 -0400

    Update et_xml.py
    Made Python3 friendly.

--- et_xml.py
+++ et_xml.py
@@ -3,17 +3,15 @@
 # built-in module to parse XML; includes some xpath support
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('/tmp/tools.nessus')
+tree = ET.parse('/root/tools.nessus')
 
 root = tree.getroot()
 
-# get all report hosts -> needed to extract IP address
 for host in root[1].findall('./ReportHost'):
-    # get all report items and report plugins that are not Info
     for item in host.findall('./ReportItem'):
         if item.find('risk_factor').text != "None":
-            print "Host IP: {}".format(host.attrib['name'])
-            print "Plugin ID: {}".format(item.attrib['pluginID'])
-            print "Plugin Name: {}".format(item.attrib['pluginName'])
-            print "Severity: {}".format(item.find('risk_factor').text)
-            print ""
+            print("Host IP: {}".format(host.get('name')))
+            print("Plugin ID: {}".format(item.get('pluginID')))
+            print("Plugin Name: {}".format(item.get('pluginName')))
+            print("Severity: {}".format(item.find('risk_factor').text))
+            print("")
